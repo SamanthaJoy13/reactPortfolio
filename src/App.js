71432db116da021693cd80328684f77916f09ca2
SamanthaJoy13 <tavaresjsamantha52@icloud.com>
2023-07-19 18:54:08 -0700

    finished resume

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter as Router} from 'react-router-dom';
 
 import Header from './components/header';
 import AboutMe from './components/about/aboutMe';
-import Contact from './components/contact';
-import Footer from './components/footer';
 import Portfolio from './components/portfolio/portfolio';
+import Contact from './components/contact';
+import Resume from './components/resume';
+import Footer from './components/footer';
+
+
 
 
 function App() {
@@ -17,6 +20,7 @@ function App() {
         <AboutMe />
         <Portfolio />
         <Contact />
+        <Resume />
         <Footer />
       </div>
     </Router>
